157604ed52e005d50fb9e9789a4eb5488aadc99e
Garrett Yien <yien.garrett94@gmail.com>
2021-08-19 01:27:56 -0400

    added editmovieform component to app.js with props

--- client/src/App.js
+++ client/src/App.js
@@ -44,7 +44,7 @@ const App = (props) => {
           <FavoriteMovieList favoriteMovies={favoriteMovies}/>
         
           <Switch>
-            <Route path="/movies/edit/:id">
+            <Route path="/movies/edit/:id" render={props => <EditMovieForm {...props} />}>
             </Route>
 
             <Route path="/movies/:id">
